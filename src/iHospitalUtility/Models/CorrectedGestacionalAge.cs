52545c4f08c36cbdf4774708055298d9452fb03c
Allan Debastiani <allandeba@yahoo.com.br>
2025-01-02 20:14:34 -0300

    feat: return fullTerm when 40 weeks plus

--- src/iHospitalUtility/Models/CorrectedGestacionalAge.cs
+++ src/iHospitalUtility/Models/CorrectedGestacionalAge.cs
@@ -2,7 +2,19 @@ namespace iHospitalUtility.Models
 {
     public class CorrectedGestacionalAge
     {
+        const int FullTermWeeks = 40;
+
         public int CorrectedWeeks { get; init; }
         public int CorrectedDays { get; init; }
+
+        public string DisplayResult()
+        {
+            var isFullTerm = CorrectedWeeks >= FullTermWeeks;
+            if (!isFullTerm)
+                return $"{CorrectedWeeks} semanas e {CorrectedDays} dias";
+
+            var totalDaysAfterFullTerm = CorrectedDays + (CorrectedWeeks - FullTermWeeks);
+            return $"{FullTermWeeks} semanas a termo e {totalDaysAfterFullTerm} dias";
+        }
     }
 }
